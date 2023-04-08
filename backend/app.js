d206f8dcd36201b74445bb5993324fb42572e884
Svetlana Galkina <G.lana08@yandex.ru>
2023-04-08 22:34:19 +0300

    Проектная работа 15

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ const { STATUS_INTERNAL_SERVER_ERROR, STATUS_NOT_FOUND } = require('./utils/cons
 const { validateLogin, validateCreateUser } = require('./middlewares/validation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 app.use(bodyParser.json());
